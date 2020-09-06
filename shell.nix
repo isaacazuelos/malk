aeb7a28564a8a9eaab916adb41fa13b49de0d189
Isaac Azuelos <isaac@azuelos.ca>
2020-09-05 18:21:00 -0600

    Fix note in nix shell and add fmt

--- shell.nix
+++ shell.nix
@@ -1,23 +1,22 @@
-let 
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+let
+  moz_overlay = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
 
   avrlibc = nixpkgs.pkgsCross.avr.buildPackages.libcCross;
-in
 
-nixpkgs.mkShell {
+in nixpkgs.mkShell {
   name = "malk";
 
   buildInputs = with nixpkgs; [
-    # As far as I can tell, this would be the _right_ way, but it
-    # doesn't work. Which is why rustup is enables instead.
-    #rustup # don't forget to use `cargo +nightly` if you have stable installed.
+    # When updating, check availablility of extensions at
+    # https://rust-lang.github.io/rustup-components-history
     ((rustChannelOf {
-     date = "2020-08-01";
-     channel = "nightly";
+      date = "2020-08-27";
+      channel = "nightly";
     }).rust.override {
-     extensions = [ "rust-src" ];
-     targetExtensions = [ "rust-src" ];
+      extensions = [ "rust-src" "rustfmt-preview" ];
+      targetExtensions = [ "rust-src" ];
     })
     pkgsCross.avr.buildPackages.gcc8
     avrlibc
